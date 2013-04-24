0aae3cb12b91ec3ac5ad9f96d7bd87af6839e1e2
Ozan Turgut <ozanturgut@gmail.com>
2013-04-24 01:47:25 -0700

    Added comments to example
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- examples/ping-pong/pong.js
+++ examples/ping-pong/pong.js
@@ -1,13 +1,19 @@
-// Listener listens to every message from each connection made to it
 var pongingAnarch = Anarch.create();
 
+// First, establish a connection to the onramp which will 
+// help us connect to other browsers
 var webSocketConnection2 = pongingAnarch.to('ws://127.0.0.1:20500/');
+
+// Whenever an RTC channel is established, call the handler function
 webSocketConnection2.on('connection', handleRtcConnection);
 
 function handleRtcConnection(rtcConnection){
+	// When ever another browser connects, listen for messages
 	rtcConnection.on('message', function(message){
+		// Output the message
 		console.log('Got: ' + message);
 
+		// Wait one second then respond with a pong
 		setTimeout(function(){
 			rtcConnection.send('Pong!');
 		}, 1000);
